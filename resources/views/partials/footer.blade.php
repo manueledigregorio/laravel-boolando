05f06a65e804e6803f4417f897fdac4ea5f9f6f6
manueledigregorio <dig.manuele@gmail.com>
2023-11-18 16:53:39 +0100

    [add] footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="container  ">
+
+        <div>
+            <h3>Booleando S.r.l.</h3>
+            <span>Informazioni legali</span>
+            <span>informativa sulla privacy</span>
+            <span>Diritto di recesso</span>
+        </div>
+        <div>
+            <span>Trovaci anche su</span>
+            <ul>
+                <li><a><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a><i class="fa-brands fa-square-facebook"></i>
+                </a></li>
+                <li><a><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a><i class="fa-brands fa-pinterest"></i></a></li>
+                <li><a><i class="fa-brands fa-youtube"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
